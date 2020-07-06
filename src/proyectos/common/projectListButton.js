bb28c947bc64fb0d98a378680e02715d5ec88099
LeviMatias <matilevi35@gmail.com>
2020-07-06 16:34:21 -0300

    basic project selection and screen transition

--- src/proyectos/common/projectListButton.js
+++ src/proyectos/common/projectListButton.js
@@ -1,27 +1,16 @@
 import React from 'react';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
-import ButtonBase from '@material-ui/core/ButtonBase';
 import { Button } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import LinearProgress from '@material-ui/core/LinearProgress';
 
 
 const useStyles = makeStyles((theme) => ({
-    paper: {
+    button: {
       padding: theme.spacing(2),
       margin: 'auto',
       width: '100%',
-    },
-    image: {
-      width: 128,
-      height: 128,
-    },
-    img: {
-      margin: 'auto',
-      display: 'block',
-      maxWidth: '100%',
-      maxHeight: '100%',
     },
   }));
 
@@ -29,13 +18,8 @@ export default (props) =>{
         const classes = useStyles();
 
         return (
-            <Button className={classes.paper} variant='outlined'>
+            <Button className={classes.button} variant='outlined' onClick={()=> props.onClick(props)} >
                 <Grid container spacing={2}>
-                <Grid item>
-                    <ButtonBase className={classes.image}>
-                    <img className={classes.img} alt="imagen o grafico" src="/static/images/grid/imagen.jpg" />
-                    </ButtonBase>
-                </Grid>
                 <Grid item xs={12} sm container>
                     <Grid item xs container direction="column" spacing={2}>
                     <Grid item xs>
--- src/views/Projects.js
+++ src/views/Projects.js
@@ -1,30 +1,54 @@
 import React, {Fragment} from 'react';
 import ContentWrapper from "components/common/ContentWrapper";
-import MetaInfo from "components/common/MetaInfo";
 import { makeStyles } from '@material-ui/core/styles';
-import ProjectListButton from "components/projects/projectListButton";
+import {useHistory, useRouteMatch} from "react-router";
+import ProjectListButton from "proyectos/common/projectListButton";
+import AnimatedSwitch from "components/common/AnimatedSwitch";
+import AnimatedRoute from "components/common/AnimatedRoute";
+import Paper from "@material-ui/core/Paper";
+import ProjectView from "proyectos/common/projectView"
 
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
   },
+  paper: {
+    padding: theme.spacing(5),
+    [theme.breakpoints.only('xs')]: {
+        padding: theme.spacing(2),
+    }
+}
 }));
 
 export default () => {
     const classes = useStyles();
-    
+    const { path } = useRouteMatch() || {};
+    const history = useHistory();
+    var projectViewProps = {}; 
+
+    const viewProject = (projectProps)=> {
+      projectViewProps = projectProps;
+      history.push(`${path}/detalles`);
+    }
+
     return (
         <Fragment>
-            <MetaInfo
-                title={'PSA - Proyectos'}
-                description='Modulo de proyectos'
-            />
             <ContentWrapper>
-                Proyectos
-                <div className={classes.root}>
-                <ProjectListButton nombre = "Nombre Proyecto" number = {1}> </ProjectListButton>
-                <ProjectListButton nombre = "Nombre Proyecto 2" number = {2}> </ProjectListButton>
-                </div>
+            <AnimatedSwitch>
+                <AnimatedRoute exact path={path}>
+                    <Paper className={classes.paper}>
+                        Proyectos
+                          <div className={classes.root}>
+                          <ProjectListButton nombre = "Nombre Proyecto" number = {1} onClick = {viewProject}> </ProjectListButton>
+                          <ProjectListButton nombre = "Nombre Proyecto 2" number = {2} onClick = {viewProject}> </ProjectListButton>
+                          </div>
+                        <br/>
+                    </Paper>
+                </AnimatedRoute>
+                <AnimatedRoute path={`${path}/detalles`}>
+                  <ProjectView projectInfo = {projectViewProps}></ProjectView>
+                </AnimatedRoute>
+            </AnimatedSwitch>
             </ContentWrapper>
         </Fragment>
     )
