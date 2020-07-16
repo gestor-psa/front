98e8a19eb3ae1730702aecf8e5bb4d87060e2366
LeviMatias <matilevi35@gmail.com>
2020-07-16 01:02:39 -0300

    proyect details view

--- src/proyectos/VerProyecto.js
+++ src/proyectos/VerProyecto.js
@@ -1,25 +1,14 @@
 import React, { useEffect, useState } from 'react';
-import Paper from "@material-ui/core/Paper";
-import Grid from "@material-ui/core/Grid";
-import Typography from "@material-ui/core/Typography";
-import { makeStyles } from "@material-ui/core/styles";
+import ProyectoDetails from "proyectos/ProyectoDetalles";
 import { useParams, useRouteMatch } from "react-router";
 import AnimatedSwitch from "components/common/AnimatedSwitch";
 import AnimatedRoute from "components/common/AnimatedRoute";
 import axios from "axios";
 
 
-const useStyles = makeStyles(theme => ({
-    root: {
-        padding: theme.spacing(4, 6),
-        [theme.breakpoints.down('xs')]: {
-            padding: theme.spacing(2, 2)
-        }
-    }
-}));
+
 
 export default () => {
-    const classes = useStyles();
     const { id } = useParams();
     const { path } = useRouteMatch() || {};
     const [proyecto, setProyecto] = useState();
@@ -39,15 +28,7 @@ export default () => {
 
         <AnimatedSwitch>
             <AnimatedRoute exact path={path}>
-                <Paper className={classes.root}>
-                    <Grid container spacing={3}>
-                        <Grid item xs={12}>
-                            {proyecto && < Typography variant='h4'>
-                                {proyecto.nombre}
-                            </Typography> }
-                        </Grid>
-                    </Grid>
-                </Paper>
+                <ProyectoDetails proyecto = {proyecto} />
             </AnimatedRoute>
             <AnimatedRoute exact path={`${path}/modificacion`}>
 
